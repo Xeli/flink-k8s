50d2d5d0737abaffb061647f9d521e665c611aae
Richard Deurwaarder <rdeurwaarder@bol.com>
2018-07-15 14:09:57 +0200

    Add 2 docker images - one for the job and taskmanager and one to submit jobs

---
+++ docker/flink-base/docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+###############################################################################
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+if [ "$1" = "jobmanager" ]; then
+    echo "Starting Job Manager"
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+
+    exec "$FLINK_HOME/bin/jobmanager.sh" start-foreground cluster
+elif [ "$1" = "job-start" ]; then
+    while [ "$(curl -s -o /dev/null -w '%{http_code}' localhost:8081/overview)" != "200" ]; do
+        echo "JobManager not yet up..."
+        sleep 1
+    done
+    exec "$FLINK_HOME/bin/flink" run /opt/deployable.jar
+elif [ "$1" = "taskmanager" ]; then
+    TASK_MANAGER_NUMBER_OF_TASK_SLOTS=${TASK_MANAGER_NUMBER_OF_TASK_SLOTS:-$(grep -c ^processor /proc/cpuinfo)}
+    sed -i -e "s/taskmanager.numberOfTaskSlots: 1/taskmanager.numberOfTaskSlots: $TASK_MANAGER_NUMBER_OF_TASK_SLOTS/g" "$FLINK_HOME/conf/flink-conf.yaml"
+    echo "Starting Task Manager"
+    echo "config file: " && grep '^[^\n#]' "$FLINK_HOME/conf/flink-conf.yaml"
+
+    exec "$FLINK_HOME/bin/taskmanager.sh" start-foreground
+fi
+
+exec "$@"
